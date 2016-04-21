82695ddf07d36abbe113a06799aa37ae01c44f47
aluno <aluno@micro000>
2016-04-20 21:55:14 -0300

    listagem dos filmes

--- src/AppBundle/Controller/FilmesController.php
+++ src/AppBundle/Controller/FilmesController.php
@@ -10,25 +10,44 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+Use AppBundle\Entity\Filmes;
+
 class FilmesController extends Controller
 {
     /**
      * @Route("/filmes", name="filmes_index")
      */
-    
+ 
     
     public function indexAction() 
     {
-        return $this->render('filmes/index.html.twig');
+        
+        $doctrine = $this->getDoctrine()->getEntityManager();
+        $filmes = $doctrine->getRepository('AppBundle:Filmes');
+        
+        $retorno = $filmes->findAll();
+        
+       
+        
+        return $this->render('filmes/index.html.twig',
+                array('filmes' => $retorno)
+         );
         
     }
      /**
-     * @Route("/filmes/cadastro")
+     * @Route("/filmes/cadastro", name="filmes_cadastro");
      */
-    
-    
     public function cadastroAction() 
     {
+        $filme = new Filmes();
+        $filme->setGenero('Terror');
+        $filme->setLancamento(true);
+        $filme->setNome('REC');
+                
+        $doctrine = $this->getDoctrine()->getEntityManager();
+        $doctrine->persist($filme);
+        $doctrine->flush();
+        
          return $this->render('filmes/cadastro.html.twig');
     }
 }
